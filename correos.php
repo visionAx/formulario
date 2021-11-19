2995da4a52cbaf831708fc22edfe36c26e0ec00a
visionax <axcel.salgadogvwint@gmail.com>
2021-11-19 08:45:20 -0600

    mi primer commit

---
+++ correos.php
@@ -0,0 +1,16 @@
+<?php
+    $destinatario = 'axcel.salgado85@gmail.com';
+
+    $nombre = $_POST['nombre'];
+    $asunto = $_POST['asunto'];
+    $mensaje = $_POST['mensaje'];
+    $email = $_POST['email'];
+
+    $header = "Enviado desde la pagina Ax"
+    $mensajeCompleto = $mensaje . "\nAtentamente: " . $nombre;
+
+    mail($destinatario, $asunto, $mensajeCompleto, $header);
+    echo "‹script›alert ('correo enviado exitosamente')‹/script›";
+    echo "‹script› setTimeout(\"location.href='index.html'\",1000) ‹/script›";
+
+?>
